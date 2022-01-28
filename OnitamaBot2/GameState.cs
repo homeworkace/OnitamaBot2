fe93877a57de45fc6f8d3e26c23a17d8dd43f0b4
Homeworkace <rickchongrockching@gmail.com>
2022-01-28 22:41:19 +0800

    AI is actually decent and you can play against it now. - Tentative state evaluation algorithm. - Min-maxing score and playing to "not lose". - Rearranged sequence of functions to alphabetical order.
    Updated priorities can be found in "./OnitamaBot2/Resources/Priorities.txt".

--- OnitamaBot2/GameState.cs
+++ OnitamaBot2/GameState.cs
@@ -17,6 +17,7 @@ namespace OnitamaBot2
 	struct Card
 	{
 		public List<short> moveList = new();
+		public string name = "";
 
 		public Card(short id)
 		{
@@ -26,87 +27,180 @@ namespace OnitamaBot2
 					moveList.Add(-1);
 					moveList.Add(1);
                     moveList.Add(5);
+					name = "Boar";
 					break;
 				case 1: //Cobra
 					moveList.Add(-4);
 					moveList.Add(-1);
 					moveList.Add(6);
+					name = "Cobra";
 					break;
 				case 2: //Crab
 					moveList.Add(-2);
 					moveList.Add(2);
 					moveList.Add(5);
+					name = "Crab";
 					break;
 				case 3: //Crane
 					moveList.Add(-6);
 					moveList.Add(-4);
 					moveList.Add(5);
+					name = "Crane";
 					break;
 				case 4: //Dragon
 					moveList.Add(-6);
 					moveList.Add(-4);
 					moveList.Add(3);
 					moveList.Add(7);
+					name = "Dragon";
 					break;
 				case 5: //Eel
 					moveList.Add(-6);
 					moveList.Add(1);
 					moveList.Add(4);
+					name = "Eel";
 					break;
 				case 6: //Elephant
 					moveList.Add(-1);
 					moveList.Add(1);
 					moveList.Add(4);
 					moveList.Add(6);
+					name = "Elephant";
 					break;
 				case 7: //Frog
 					moveList.Add(-4);
 					moveList.Add(-2);
 					moveList.Add(4);
+					name = "Frog";
 					break;
 				case 8: //Goose
 					moveList.Add(-4);
 					moveList.Add(-1);
 					moveList.Add(1);
 					moveList.Add(4);
+					name = "Goose";
 					break;
 				case 9: //Horse
 					moveList.Add(-5);
 					moveList.Add(-1);
 					moveList.Add(5);
+					name = "Horse";
 					break;
 				case 10: //Mantis
 					moveList.Add(-5);
 					moveList.Add(4);
 					moveList.Add(6);
+					name = "Mantis";
 					break;
 				case 11: //Monkey
 					moveList.Add(-6);
 					moveList.Add(-4);
 					moveList.Add(4);
 					moveList.Add(6);
+					name = "Monkey";
 					break;
 				case 12: //Ox
 					moveList.Add(-5);
 					moveList.Add(1);
 					moveList.Add(5);
+					name = "Ox";
 					break;
 				case 13: //Rabbit
 					moveList.Add(-6);
 					moveList.Add(2);
 					moveList.Add(6);
+					name = "Rabbit";
 					break;
 				case 14: //Rooster
 					moveList.Add(-6);
 					moveList.Add(-1);
 					moveList.Add(1);
 					moveList.Add(6);
+					name = "Rooster";
 					break;
 				case 15: //Tiger
 					moveList.Add(-5);
 					moveList.Add(10);
+					name = "Tiger";
 					break;
 			}
 		}
+	}
+	class Evaluator
+	{
+		bool IsOplayer = false;
+		int[] posX = new int[10];
+		int[] posY = new int[10];
+
+		public double Evaluate(GameState state)
+		{
+			//tentative algorithm:
+			//1. sum of distances to enemy daimyo - sum of enemy distances to daimyo
+			//2. daimyo distance to enemy shrine - enemy daimyo distance to shrine
+			//X win = +1000, O win = -1000
+			if (Game.CheckWin(state) != 0)
+				return Game.CheckWin(state) * (IsOplayer ? -1000 : 1000);
+
+			//work out the position of every piece in x-y coordinates
+			for (int i = 0; i < 10; i++)
+            {
+				posX[i] = state.position[i] % 5;
+				posY[i] = state.position[i] / 5;
+			}
+
+			double result = 0;
+            float distance;
+            for (int i = 0; i < 5; ++i)
+            {
+				//+ sum of (5 / X distance to O daimyo)
+				distance = (posX[7] - posX[i]) * (posX[7] - posX[i]) + (posY[7] - posY[i]) * (posY[7] - posY[i]);
+				//result += (IsOplayer? -5 : 5) / distance;
+				result += 5 / distance;
+				//- sum of (5 / O distance to X daimyo)
+				distance = (posX[2] - posX[i + 5]) * (posX[2] - posX[i + 5]) + (posY[2] - posY[i + 5]) * (posY[2] - posY[i + 5]);
+				//result -= (IsOplayer ? -5 : 5) / distance;
+				result -= 5 / distance;
+			}
+			//+ 3 / X daimyo distance to O shrine
+			distance = (2 - posX[2]) * (2 - posX[2]) + (4 - posY[2]) * (4 - posY[2]);
+			//result += (IsOplayer ? -3 : 3) / distance;
+			result += 3 / distance;
+			//+ 3 / O daimyo distance to X shrine
+			distance = (2 - posX[7]) * (2 - posX[7]) + (0 - posY[7]) * (0 - posY[7]);
+			//result -= (IsOplayer ? -3 : 3) / distance;
+			result -= 3 / distance;
+
+			return result;
+		}
+
+		public void EvaluateFrom(GameState state)
+        {
+			List<short> indices = new();
+			GameState queryState = state;
+
+			//if there's a child, go down
+			indices.Add(0);
+			while (state.children.Count != indices[0])
+				if (queryState.children.Count > indices.Last())
+				{
+					queryState = queryState.children[indices.Last()];
+					indices.Add(0);
+				}
+				//if there's no child, evaluate and go back up
+				else
+				{
+					if (queryState.children.Count == 0)
+						queryState.score = Evaluate(queryState);
+					if (indices[indices.Count - 2] == 0)
+						queryState.parent.score = queryState.score;
+					else if (queryState.Oturn % 2 == 0 && queryState.score < queryState.parent.score)
+						queryState.parent.score = queryState.score;
+					else if (queryState.Oturn % 2 == 1 && queryState.score > queryState.parent.score)
+						queryState.parent.score = queryState.score;
+					queryState = queryState.parent;
+					indices.RemoveAt(indices.Count - 1);
+					indices[indices.Count - 1] += 1;
+				}
+        }
 	}
 }
--- OnitamaBot2/Program.cs
+++ OnitamaBot2/Program.cs
@@ -8,207 +8,7 @@ class Game
 	GameState currentState;
 	Card[] cardList = new Card[5];
 	Queue<GameState> jobQueue = new();
-	public void TheGame()
-	{
-		GameState currentState = NewGame();
-
-		if (CheckWin(currentState) != 0)
-		{
-			//eventCode = 1;
-			//for (short i = 0; i < 8; ++i)
-			//worker[i].join();
-		}
-		short theMove = 1;
-		WeakReference theTest = null;
-		//thread worker = thread(MCSearch, &theMove);
-		//Console.Write("Computing your moves... Type 0 to stop the calculations or type anything else to view the current scores.\n");
-		/*while (true)
-		{
-			short.TryParse(Console.ReadLine(), out theMove);
-			Console.Write("Scores:\n");
-			for (short i = 0; i < currentState.children.Count; ++i)
-				//cout << (float)i->wins / i->playouts << "% (" << i->wins << "/" << i->playouts << ")\n";
-				Console.WriteLine(i + ": " + currentState.children[i].score);
-			if (theMove == 0)
-				break;
-		}*/
-		//worker.join();
-
-		while (CheckWin(currentState) == 0)
-		{
-			if (currentState.Oturn % 2 == 1)
-				Console.Write("O");
-			else Console.Write("X");
-			Console.Write(", make your next move: ");
-			while (!short.TryParse(Console.ReadLine(), out theMove))
-				Console.Write("Please enter a number: ");
-			if (theMove == -1) //Dump state tree.
-			{
-				Console.Write("Tree dump:\n");
-				TreeDump();
-			}
-			else if (theMove == -2) //Go back one move.
-			{
-				if (currentState.parent is not null)
-					currentState = currentState.parent;
-			}
-			else if (theMove == -3) //Something about worker threads.
-			{
-				//eventCode = 2;
-			}
-			else if (theMove == -4) //Something about worker threads.
-			{
-				//eventCode = 0;
-			}
-			else if (theMove == -5) //Print all possible options
-			{
-				for (int i = 0; i < currentState.children.Count; ++i)
-                {
-					Console.WriteLine(i + ": " + Print(currentState.children[i]));
-					PrintBoard(currentState.children[i]);
-				}
-			}
-			else if (theMove == -6) //the debug command
-			{
-			}
-			else if (theMove >= currentState.children.Count || theMove < 0)
-			{
-				Console.Write("Not an option.\n");
-			}
-			else
-			{
-				//Plan to prune tree after every move:
-				//- Set aside the branch that contains the move that was actually made.
-				GameState chosenMove = currentState.children[theMove];
-				currentState.children.Remove(chosenMove);
-				GameState pruneQuery = currentState;
-				while (currentState.children.Count > 0)
-				{
-					//- For every other branch: if it has children, go down the first child.
-					while (pruneQuery.children.Count > 0)
-						pruneQuery = pruneQuery.children[0];
-					//- If it has no children, go back up one level, get the first child to null its parent, null the first child, then remove the first child.
-					pruneQuery = pruneQuery.parent;
-					pruneQuery.children[0].parent = null;
-					pruneQuery.children[0] = null;
-					pruneQuery.children.RemoveAt(0);
-				} //- Ask the same question wherever you go.
-				currentState.children.Add(chosenMove);
-				currentState = chosenMove;
-				PrintBoard(currentState);
-				while (jobQueue.Peek().Oturn - currentState.Oturn < 4)
-				{
-					pruneQuery = jobQueue.Dequeue();
-					if (pruneQuery.parent != null)
-						CheckMoves(pruneQuery);
-					if (jobQueue.Count == 0)
-						break;
-				}
-			}
-		}
-	}
-
-	GameState NewGame()
-	{
-		int choice = 0;
-		Console.Write("X starts first.\nPlace the corresponding numbers of the 5 cards in play, side by side, in this order: X's first card, X's second card, turn card, O's first card, O's second card.\nReference:\n00: Boar\n01: Cobra\n02: Crab\n03: Crane\n04: Dragon\n05: Eel\n06: Elephant\n07: Frog\n08: Goose\n09: Horse\n10: Mantis\n11: Monkey\n12: Ox\n13: Rabbit\n14: Rooster\n15: Tiger\nExample: X starts with Boar (00) and Cobra (01), O starts with Crane (03) and Dragon (04). X always starts first, so the turn card, Crab (03) belongs to X. The input is thus: 0001020304.\n");
-		while (!int.TryParse(Console.ReadLine(), out choice)) //Goose, Rooster, Tiger, Eel, Mantis
-			Console.Write("Please enter a number: ");
-		for (short i = 0; i < 5; ++i)
-			cardList[i] = new Card((short)((int)(choice / Math.Pow(10, 8 - i * 2)) % 100)); //To isolate the 2 digits we want, divide by a power of ten then floor to get rid of every digit to the right, then mod 100 to get rid of every digit to the left.
-
-		GameState pos = new();
-		pos.Oturn = 0;
-		pos.parent = null;
-		pos.position[0] = 0;
-		pos.position[1] = 1;
-		pos.position[2] = 2;
-		pos.position[3] = 3;
-		pos.position[4] = 4;
-		pos.position[5] = 20;
-		pos.position[6] = 21;
-		pos.position[7] = 22;
-		pos.position[8] = 23;
-		pos.position[9] = 24;
-		pos.position[10] = 0;
-		pos.position[11] = 1;
-		pos.position[12] = 2;
-		pos.position[13] = 3;
-		pos.position[14] = 4;
-		currentState = pos;
-
-		CheckMoves(currentState);
-		while (jobQueue.Peek().Oturn < 4)
-			CheckMoves(jobQueue.Dequeue());
-		return currentState;
-	}
-
-	string Print(GameState state)
-	{
-		string result = "";
-		foreach (int i in state.position)
-			result += i + ", ";
-		return result;
-	}
-
-	void PrintBoard(GameState state)
-	{
-		Console.Write("_______\n");
-		for (short y = 0; y < 5; ++y)
-		{
-			Console.Write("|");
-			for (short x = 0; x < 5; ++x)
-			{
-				short i = 0;
-				for (; i < 10; ++i)
-				{
-					if (state.position[i] == (4 - y) * 5 + x)
-					{
-						if (i < 5)
-						{
-							if (i == 2)
-								Console.Write("X");
-							else Console.Write("x");
-						}
-						else if (i == 7)
-							Console.Write("O");
-						else Console.Write("o");
-						break;
-					}
-				}
-				if (i == 10)
-					Console.Write(" ");
-			}
-			Console.Write("|\n");
-		}
-		Console.Write("_______\n");
-	}
-
-    static short CheckWin(GameState curState)
-	{
-		if (curState.position[7] < 0)
-		{
-			Console.Write("X Stone.\n");
-			return 1;
-		}
-		else if (curState.position[2] == 22)
-		{
-			Console.Write("X Stream.\n");
-			return 1;
-		}
-		else if (curState.position[2] < 0)
-		{
-			Console.Write("O Stone.\n");
-			return -1;
-		}
-		else if (curState.position[7] < 2)
-		{
-			Console.Write("O Stream.\n");
-			return -1;
-		}
-		else return 0;
-	}
-
+	Evaluator theBot = new Evaluator();
 	void CheckMoves(GameState curState)
 	{
 		//for each piece
@@ -243,7 +43,7 @@ class Game
 				GameState pos = new();
 				pos.Oturn = (short)(curState.Oturn + 1);
 				pos.parent = curState;
-				pos.score = 100;
+				pos.score = 0;
 				for (k = 0; k < 15; ++k)
 					pos.position[k] = curState.position[k];
 				pos.position[i + (curState.Oturn % 2) * 5] = (short)result;
@@ -257,8 +57,9 @@ class Game
 				pos.position[12] = pos.position[10 + (curState.Oturn % 2) * 3];
 				pos.position[10 + (curState.Oturn % 2) * 3] = swapCard;
 				curState.children.Add(pos);
-				//if (pos.Oturn - currentState.Oturn < 4)
+				if (CheckWin(pos) == 0)
 					jobQueue.Enqueue(pos);
+				else pos.score = CheckWin(pos) * 1000;
 				//Console.WriteLine(curState.children.Count - 1);
 				//PrintBoard(pos);
 				//Console.WriteLine(Print(pos));
@@ -289,7 +90,7 @@ class Game
 				GameState pos = new();
 				pos.Oturn = (short)(curState.Oturn + 1);
 				pos.parent = curState;
-				pos.score = 100;
+				pos.score = 0;
 				for (k = 0; k < 15; ++k)
 					pos.position[k] = curState.position[k];
 				pos.position[i + (curState.Oturn % 2) * 5] = (short)result;
@@ -303,11 +104,222 @@ class Game
 				pos.position[12] = pos.position[11 + (curState.Oturn % 2) * 3];
 				pos.position[11 + (curState.Oturn % 2) * 3] = swapCard;
 				curState.children.Add(pos);
-				//if (pos.Oturn < 4)
+				if (CheckWin(pos) == 0)
 					jobQueue.Enqueue(pos);
+				else pos.score = CheckWin(pos) * 1000;
 				//Console.WriteLine(curState.children.Count - 1);
 				//PrintBoard(pos);
 				//Console.WriteLine(Print(pos));
+			}
+		}
+	}
+	static public short CheckWin(GameState curState)
+	{
+		if (curState.position[7] < 0)
+		{
+			//Console.Write("X Stone.\n");
+			return 1;
+		}
+		else if (curState.position[2] == 22)
+		{
+			//Console.Write("X Stream.\n");
+			return 1;
+		}
+		else if (curState.position[2] < 0)
+		{
+			//Console.Write("O Stone.\n");
+			return -1;
+		}
+		else if (curState.position[7] < 2)
+		{
+			//Console.Write("O Stream.\n");
+			return -1;
+		}
+		else return 0;
+	}
+	GameState NewGame()
+	{
+		int choice = 0;
+		Console.Write("X starts first.\nPlace the corresponding numbers of the 5 cards in play, side by side, in this order: X's first card, X's second card, turn card, O's first card, O's second card.\nReference:\n00: Boar\n01: Cobra\n02: Crab\n03: Crane\n04: Dragon\n05: Eel\n06: Elephant\n07: Frog\n08: Goose\n09: Horse\n10: Mantis\n11: Monkey\n12: Ox\n13: Rabbit\n14: Rooster\n15: Tiger\nExample: X starts with Boar (00) and Cobra (01), O starts with Crane (03) and Dragon (04). X always starts first, so the turn card, Crab (03) belongs to X. The input is thus: 0001020304.\n");
+		while (!int.TryParse(Console.ReadLine(), out choice)) //Crane, Crab, Tiger, Mantis, Elephant
+			Console.Write("Please enter a number: ");
+		for (short i = 0; i < 5; ++i)
+			cardList[i] = new Card((short)((int)(choice / Math.Pow(10, 8 - i * 2)) % 100)); //To isolate the 2 digits we want, divide by a power of ten then floor to get rid of every digit to the right, then mod 100 to get rid of every digit to the left.
+
+		GameState pos = new();
+		pos.Oturn = 0;
+		pos.parent = null;
+		pos.score = 0;
+		pos.position[0] = 0;
+		pos.position[1] = 1;
+		pos.position[2] = 2;
+		pos.position[3] = 3;
+		pos.position[4] = 4;
+		pos.position[5] = 24;
+		pos.position[6] = 23;
+		pos.position[7] = 22;
+		pos.position[8] = 21;
+		pos.position[9] = 20;
+		pos.position[10] = 0;
+		pos.position[11] = 1;
+		pos.position[12] = 2;
+		pos.position[13] = 3;
+		pos.position[14] = 4;
+		currentState = pos;
+
+		CheckMoves(currentState);
+		while (jobQueue.Peek().Oturn < 4)
+			CheckMoves(jobQueue.Dequeue());
+		theBot.EvaluateFrom(currentState);
+		return currentState;
+	}
+	string Print(GameState state)
+	{
+		string result = "";
+		foreach (int i in state.position)
+			result += i + ", ";
+		//for (int i = 10; i < 15; ++i)
+		//	result += state.position[i] + ", ";
+		result.Remove(result.Length - 2, 2);
+		return result;
+	}
+	void PrintBoard(GameState state)
+	{
+		Console.Write("_______\n");
+		for (short y = 0; y < 5; ++y)
+		{
+			Console.Write("|");
+			for (short x = 0; x < 5; ++x)
+			{
+				short i = 0;
+				for (; i < 10; ++i)
+				{
+					if (state.position[i] == (4 - y) * 5 + x)
+					{
+						if (i < 5)
+						{
+							if (i == 2)
+								Console.Write("X");
+							else Console.Write("x");
+						}
+						else if (i == 7)
+							Console.Write("O");
+						else Console.Write("o");
+						break;
+					}
+				}
+				if (i == 10)
+					Console.Write(" ");
+			}
+			Console.Write("|\n");
+		}
+		Console.Write("_______\n");
+	}
+	public void TheGame()
+	{
+		GameState currentState = NewGame();
+
+		if (CheckWin(currentState) != 0)
+		{
+			//eventCode = 1;
+			//for (short i = 0; i < 8; ++i)
+			//worker[i].join();
+		}
+		short theMove = 1;
+		//thread worker = thread(MCSearch, &theMove);
+		//Console.Write("Computing your moves... Type 0 to stop the calculations or type anything else to view the current scores.\n");
+		/*while (true)
+		{
+			short.TryParse(Console.ReadLine(), out theMove);
+			Console.Write("Scores:\n");
+			for (short i = 0; i < currentState.children.Count; ++i)
+				//cout << (float)i->wins / i->playouts << "% (" << i->wins << "/" << i->playouts << ")\n";
+				Console.WriteLine(i + ": " + currentState.children[i].score);
+			if (theMove == 0)
+				break;
+		}*/
+		//worker.join();
+
+		while (CheckWin(currentState) == 0)
+		{
+			if (currentState.Oturn % 2 == 1)
+				Console.Write("O");
+			else Console.Write("X");
+			Console.Write(", make your next move: ");
+			while (!short.TryParse(Console.ReadLine(), out theMove))
+				Console.Write("Please enter a number: ");
+			if (theMove == -1) //Dump state tree.
+			{
+				Console.Write("Tree dump:\n");
+				TreeDump();
+			}
+			else if (theMove == -2) //Go back one move.
+			{
+				if (currentState.parent is not null)
+					currentState = currentState.parent;
+			}
+			else if (theMove == -3) //Print the suggested move.
+			{
+				int bestCandidate = 0;
+				for (int i = 1; i < currentState.children.Count; ++i)
+                {
+					if (currentState.Oturn % 2 == 0 && currentState.children[i].score > currentState.children[bestCandidate].score)
+						bestCandidate = i;
+					else if (currentState.Oturn % 2 == 1 && currentState.children[i].score < currentState.children[bestCandidate].score)
+						bestCandidate = i;
+				}
+				Console.WriteLine(bestCandidate + " use " + cardList[currentState.children[bestCandidate].position[12]].name + ": " + currentState.children[bestCandidate].score);
+				PrintBoard(currentState.children[bestCandidate]);
+			}
+			else if (theMove == -4) //Something about worker threads.
+			{
+				//eventCode = 0;
+			}
+			else if (theMove == -5) //Print all possible options
+			{
+				for (int i = 0; i < currentState.children.Count; ++i)
+                {
+					Console.WriteLine(i + ": " + Print(currentState.children[i]) + " / " + currentState.children[i].score);
+					PrintBoard(currentState.children[i]);
+				}
+			}
+			else if (theMove == -6) //the debug command
+			{
+			}
+			else if (theMove >= currentState.children.Count || theMove < 0)
+			{
+				Console.Write("Not an option.\n");
+			}
+			else
+			{
+				//PRUNING: Set aside the branch that contains the move that was actually made.
+				GameState chosenMove = currentState.children[theMove];
+				currentState.children.Remove(chosenMove);
+				GameState stateQuery = currentState;
+				while (currentState.children.Count > 0)
+				{
+					//For every other branch: if it has children, go down the first child.
+					while (stateQuery.children.Count > 0)
+						stateQuery = stateQuery.children[0];
+					//If it has no children, go back up one level, get the first child to null its parent, null the first child, then remove the first child.
+					stateQuery = stateQuery.parent;
+					stateQuery.children[0].parent = null;
+					stateQuery.children[0] = null;
+					stateQuery.children.RemoveAt(0);
+				} 
+				currentState.children.Add(chosenMove);
+				currentState = chosenMove;
+				PrintBoard(currentState);
+				while (jobQueue.Peek().Oturn - currentState.Oturn < 4)
+				{
+					stateQuery = jobQueue.Dequeue();
+					//Only work on jobs for positions still on the tree.
+					if (stateQuery.parent != null)
+						CheckMoves(stateQuery);
+					if (jobQueue.Count == 0)
+						break;
+				}
+				theBot.EvaluateFrom(currentState);
 			}
 		}
 	}
@@ -324,7 +336,7 @@ class Game
 			//print
 			for (short k = 0; k < stack.Count; ++k)
 				Console.Write("| ");
-			Console.Write("|- (" + Print(curNode) + ")\n");
+			Console.Write("|- (" + Print(curNode) + ") " + curNode.score + "\n");
 			//go down
 			if (curNode.children.Count > 0)
 			{
@@ -342,10 +354,9 @@ class Game
 					stack.RemoveAt(stack.Count - 1);
 				}
 				//go right
-				curNode = curNode.parent.children.Last();
+				curNode = curNode.parent.children[stack.Last()];
 				++stack[stack.Count - 1];
 			}
 		} while (stack.Count > 0);
 	}
-
 }
